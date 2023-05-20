6b8fbf7b5671f0bef54c7d4fcb2a0899f780f156
Salevieno <salvio_almeida@hotmail.com>
2023-05-20 17:05:00 -0300

    initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import gitLogo from "../assets/github-mark.png"
+
+function App() {
+  return (
+    <div className="App">
+      <img src={gitLogo} width={72} height={72} alt={"logo github"} />
+    </div>
+  );
+}
+
+export default App;
